da1c4bb47b29536f2a3829f04643c5b83a907e84
valteramn <valter_montes@hotmail.com>
2024-09-11 20:57:52 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -14,9 +14,10 @@ st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
 st.sidebar.markdown("Desenvolvido por **Valter Montes**")
 
 
-btn = st.button("Acesse os dados no Kaggle")
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+btn = st.link_button(
+                    "Acesse os dados no Kaggle", 
+                     "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+                     )
 
 st.markdown(
     """
